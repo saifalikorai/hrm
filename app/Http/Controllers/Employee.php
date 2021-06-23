81c76f6a2bd0e46c97a81cda608214681cf550e0
Saif Ali Korai <saifalikorai@gmail.com>
2021-06-23 15:18:40 +0500

    new

---
+++ app/Http/Controllers/Employee.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Employee extends Controller
+{
+    public function index()
+    {
+    	echo "string";
+
+    	//return view('dashboard');
+    }
+}
---
+++ app/Http/Controllers/Hr.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Hr extends Controller
+{
+    public function index()
+    {
+    	# code...
+    }
+}
